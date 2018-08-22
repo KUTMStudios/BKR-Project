086664d25325f71598f9fcb496e025f19b188c55
KUTMStudios <42508441+KUTMStudios@users.noreply.github.com>
2018-08-21 22:27:51 -0500

    Add Unity Project

---
+++ Breaker Test/Assets/Scripts/Production/CameraMotor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.30f;
+    public float boundY = 0.30f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // This is to check if we are inside the bounds of the X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        // This is to check if we are inside the bounds of the Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundX)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
---
+++ Breaker Test/Assets/Scripts/Production/Player_Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+    public float Speed;
+
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        
+        
+
+        // Reset MoveDelta
+        moveDelta = new Vector3(x,y,0);
+
+        // Swap sprite direction, wether you are going right or left
+        if (moveDelta.x < 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make sure you can move in this direction by casting a box there first, if the box returns null we are free to move
+        hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Player","Blocking"));
+        if(hit.collider == null)
+        {
+            // Make this thing move moveDelta.Y
+            transform.Translate(0,moveDelta.y * Speed * Time.deltaTime,0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Player", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make this thing move moveDelta.x
+            transform.Translate(moveDelta.x * Speed * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
