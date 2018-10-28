ed622133f211c0d2a6fa1f32cb7008d02a79a8b7
KUTMStudios <42508441+KUTMStudios@users.noreply.github.com>
2018-10-27 22:50:23 -0500

    Add Chest

---
+++ Breaker Test/Assets/Scripts/Production/Chest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collidable
+{
+    protected override void OnCollide(Collider2D coll)
+    {
+        Debug.Log("Grant Pesos");
+    }
+}
