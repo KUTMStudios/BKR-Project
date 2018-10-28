7b069399acd76fc504d8d12da759cd9aeb897a60
KUTMStudios <42508441+KUTMStudios@users.noreply.github.com>
2018-10-28 01:10:39 -0600

    Teleport

---
+++ Breaker Test/Assets/Scripts/Production/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : Collidable
+{
+    public string[] sceneNames;
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player")
+        {
+            //Teleport Player
+            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+}
